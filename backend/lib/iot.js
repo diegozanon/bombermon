9ff4819da1b80b1340579ed3300264d1043c9623
diegozanon <12113544+diegozanon@users.noreply.github.com>
2020-10-05 07:54:25 -0300

    Fixing error with IoT certificates

--- backend/lib/iot.js
+++ backend/lib/iot.js
@@ -8,37 +8,37 @@ const roleName = 'bombermon-iot';
 
 module.exports.getIoTKeys = (callback) => {
 
-  // get the endpoint address
-  iotClient.describeEndpoint({}, (err, data) => {
-    if (err) return utils.errorHandler(err, callback);
-
-    const iotEndpoint = data.endpointAddress;
- 
-    // get the account id which will be used to assume a role
-    sts.getCallerIdentity({}, (err, data) => {
-      if (err) return utils.errorHandler(err, callback);
-
-      const params = {
-        RoleArn: `arn:aws:iam::${data.Account}:role/${roleName}`,
-        RoleSessionName: utils.getRandomInt(0, Number.MAX_SAFE_INTEGER).toString()
-      };
-
-      // assume role returns temporary keys
-      sts.assumeRole(params, (err, data) => {
+    // get the endpoint address
+    iotClient.describeEndpoint({ endpointType: 'iot:Data-ATS' }, (err, data) => {
         if (err) return utils.errorHandler(err, callback);
 
-        return callback(null, { 
-          statusCode: 200, 
-          headers: utils.headers, 
-          body: JSON.stringify({
-            iotEndpoint: iotEndpoint,
-            region: utils.getRegion(iotEndpoint),
-            awsAccessKey: data.Credentials.AccessKeyId,
-            awsSecretAccessKey: data.Credentials.SecretAccessKey,
-            sessionToken: data.Credentials.SessionToken
-          }) 
-        });
-      });
-    })
-  });
+        const iotEndpoint = data.endpointAddress;
+
+        // get the account id which will be used to assume a role
+        sts.getCallerIdentity({}, (err, data) => {
+            if (err) return utils.errorHandler(err, callback);
+
+            const params = {
+                RoleArn: `arn:aws:iam::${data.Account}:role/${roleName}`,
+                RoleSessionName: utils.getRandomInt(0, Number.MAX_SAFE_INTEGER).toString()
+            };
+
+            // assume role returns temporary keys
+            sts.assumeRole(params, (err, data) => {
+                if (err) return utils.errorHandler(err, callback);
+
+                return callback(null, {
+                    statusCode: 200,
+                    headers: utils.headers,
+                    body: JSON.stringify({
+                        iotEndpoint: iotEndpoint,
+                        region: utils.getRegion(iotEndpoint),
+                        awsAccessKey: data.Credentials.AccessKeyId,
+                        awsSecretAccessKey: data.Credentials.SecretAccessKey,
+                        sessionToken: data.Credentials.SessionToken
+                    })
+                });
+            });
+        })
+    });
 };
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,17 +7,17 @@ game.state.add("TiledState", new Bombermon.TiledState());
 game.state.start("BootState", true, false, "assets/level.json", "TiledState");
 
 var players = [];
-[1, 2, 3, 4, 5, 6, 7, 8].forEach(function(i) {
+[1, 2, 3, 4, 5, 6, 7, 8].forEach(function (i) {
     players.push({ x: 0, y: 0, visible: false, frame: 0 });
 });
 
 Bombermon.players = players;
 
 // config
-var apiGateway = 'https://z50m189rv0.execute-api.us-east-1.amazonaws.com/dev/'; 
+var apiGateway = 'https://abcdefghij.execute-api.us-east-1.amazonaws.com/dev/';
 
 // send message
-Bombermon.sendMessage = function(message) {
+Bombermon.sendMessage = function (message) {
     message.timestamp = new Date().getTime();
     var msgObj = {
         playerId: Bombermon.my_player_id,
@@ -28,7 +28,7 @@ Bombermon.sendMessage = function(message) {
 };
 
 // receive message
-window.handleReceivedMessage = function(message) {
+window.handleReceivedMessage = function (message) {
     const rcvMsg = JSON.parse(message.toString());
     if (rcvMsg.playerId !== Bombermon.my_player_id) {
         Bombermon.players[rcvMsg.playerId - 1] = { x: rcvMsg.message.x, y: rcvMsg.message.y, visible: rcvMsg.message.visible, bomb: rcvMsg.message.bomb, frame: rcvMsg.message.frame };
@@ -43,24 +43,24 @@ var iotTopic = '/game/pubsub';
 $.ajax({
     method: 'GET',
     url: apiGateway + 'iot/keys',
-    success: function(res) {
+    success: function (res) {
         console.log('Time to load keys (in milliseconds): ' + (new Date().getTime() - timestamp));
         IoT.connect(iotTopic, res.iotEndpoint, res.region, res.awsAccessKey, res.awsSecretAccessKey, res.sessionToken);
     }
 });
 
 // handle connection
-window.handleConnected = function() {
+window.handleConnected = function () {
     // start game
-    var timestamp = new Date().getTime();  
+    var timestamp = new Date().getTime();
     $.ajax({
         method: 'POST',
         data: JSON.stringify({ id: 0 }),
         url: apiGateway + 'avatars/available',
-        success: function(res) {
+        success: function (res) {
             console.log('Time to load avatar (in milliseconds): ' + (new Date().getTime() - timestamp));
             Bombermon.my_player_id = res.avatarId;
-            
+
             if (res.avatarId === 0) {
                 loadMaxPlayersMsg();
             }
@@ -70,18 +70,18 @@ window.handleConnected = function() {
 
 // die
 Bombermon.died_sent = false;
-Bombermon.died = function() {
+Bombermon.died = function () {
     if (!Bombermon.died_sent) {
         Bombermon.died_sent = true;
-        var timestamp = new Date().getTime();  
+        var timestamp = new Date().getTime();
         $.ajax({
             method: 'PUT',
             data: JSON.stringify({ id: Bombermon.my_player_id }),
             contentType: 'application/json',
             url: apiGateway + 'avatars/available',
-            success: function(res) {
+            success: function (res) {
                 console.log('Time to inform player death (in milliseconds): ' + (new Date().getTime() - timestamp));
-                setTimeout(function() { window.location.reload(false); }, 2000);
+                setTimeout(function () { window.location.reload(false); }, 2000);
             }
         });
 
@@ -90,7 +90,7 @@ Bombermon.died = function() {
 };
 
 // keep alive - inform that this player is still in use
-setInterval(function() {
+setInterval(function () {
     $.ajax({
         method: 'POST',
         data: JSON.stringify({ id: Bombermon.my_player_id }),
@@ -100,7 +100,7 @@ setInterval(function() {
 
 function loadMaxPlayersMsg() {
 
-    setTimeout(function() { // TODO: discover how to know when game has loaded
+    setTimeout(function () { // TODO: discover how to know when game has loaded
         var overlay = game.add.graphics();
         overlay.beginFill(0x000000, 0.5);
         overlay.drawRect(0, 0, 240, 240);
